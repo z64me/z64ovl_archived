c74cdc5cadc68dd08ff7dfdbc367e6b329ae711d
z64me <45853441+z64me@users.noreply.github.com>
2019-04-01 19:09:02 -0400

    Create oot_u_10.h

---
+++ oot_u_10.h
@@ -0,0 +1,21 @@
+#ifndef __Z64OVL_OOT_U_10_H__
+#define __Z64OVL_OOT_U_10_H__
+
+#define OOT_U_1_0 1
+
+#include "mips.h"
+#include "oot_types.h"
+#include "actor_oot.h"
+#include "z64_functions.h"
+
+// RAM Stuff
+#define RAM_SEGMENT_TABLE "TODO"
+#define GLOBAL_CONTEXT    "TODO"
+#define VIEW_CONTEXT      "TODO"
+#define AS_A0             "TODO" // spawn_actor argument 0
+#define OS_A0             "TODO" // spawn_object argument 0
+#define LINK              "TODO"
+#define NAVI              "TODO"
+#define ZTARGETPOINTER    "TODO"
+
+#endif
