1a792423dfbf005f2041f156c2bb2594597d22fb
Nick <DJInsomniaPro@gmail.com>
2019-07-12 03:09:18 -0500

    Update u10.h
    Found RAM_SEGMENT_TABLE for Majora's Mask, as well as a function that is similar to my custom rel2abs

--- mm/u10.h
+++ mm/u10.h
@@ -11,7 +11,7 @@
 // RAM Stuff
 #define SAVE_CONTEXT      0x801EF670
 #define GLOBAL_IS_NIGHT   0x801EF680 // u32, 0 if day, 1 if night
-#define RAM_SEGMENT_TABLE //TODO
+#define RAM_SEGMENT_TABLE 0x801F8180 // 0x80100504 is like rel2abs
 #define GLOBAL_CONTEXT    0x803E6B20
 #define AS_A0             (GLOBAL_CONTEXT + 0x001CA0) // spawn_actor argument 0
 #define OS_A0             (GLOBAL_CONTEXT + 0x017D88) // spawn_object argument 0
