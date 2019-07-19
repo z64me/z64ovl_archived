15747b1f642d91feb494b7d5ca80229e23c2a203
z64me <45853441+z64me@users.noreply.github.com>
2019-07-19 03:22:49 -0400

    Add type vec2s_t

--- h/various_types.h
+++ h/various_types.h
@@ -70,6 +70,12 @@ typedef struct
 
 typedef struct
 {
+	int16_t x;
+	int16_t y;
+} vec2s_t;
+
+typedef struct
+{
   float             x;                        /* 0x0000 */
   float             y;                        /* 0x0004 */
   float             z;                        /* 0x0008 */
