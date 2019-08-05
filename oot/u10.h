84afe521c0a10a49edb3b852b2b86651e3acbf51
z64me <45853441+z64me@users.noreply.github.com>
2019-08-04 21:00:36 -0400

    New offset findings courtesy of /Zel/

--- oot/u10.h
+++ oot/u10.h
@@ -10,14 +10,11 @@
 
 // RAM Stuff
 /*#define RAM_SEGMENT_TABLE "TODO"
-#define GLOBAL_CONTEXT    "TODO"
-#define VIEW_CONTEXT      "TODO"
-#define AS_A0             "TODO" // spawn_actor argument 0
-#define OS_A0             "TODO" // spawn_object argument 0
-#define LINK              "TODO"
-#define NAVI              "TODO"
-#define ZTARGETPOINTER    "TODO"*/
+#define NAVI              "TODO"*/
+#define GLOBAL_CONTEXT    0x801C84A0
 #define SAVE_CONTEXT      0x8011A5D0
 #define GLOBAL_IS_NIGHT   0x8011A5E0 // u32, 0 if day, 1 if night
+#define AS_A0             (GLOBAL_CONTEXT + 0x001C24) // spawn_actor argument 0
+#define OS_A0             (GLOBAL_CONTEXT + 0x0117A4) // spawn_object argument 0
 
 #endif
