0724b5be0bf32d5cbb220e648420588498d4870d
z64me <45853441+z64me@users.noreply.github.com>
2019-10-09 09:11:17 -0400

    move shared relative offsets into `shared.h`

--- oot/debug.h
+++ oot/debug.h
@@ -8,14 +8,14 @@
 #include "../z64_functions.h"
 
 // RAM Stuff
-#define Z64GL_INTERFACE_ON  0x8015F9BF
-#define SAVE_CONTEXT        0x8015E660
-#define GLOBAL_IS_NIGHT     0x8015E670 // u32, 0 if day, 1 if night
-#define STATIC_CONTEXT      0x8015FA90
-#define RAM_SEGMENT_TABLE   0x80166FA8
-#define GLOBAL_CONTEXT      0x80212020
-#define AS_A0               (GLOBAL_CONTEXT + 0x001C24) // spawn_actor argument 0
-#define OS_A0               (GLOBAL_CONTEXT + 0x0117A4) // spawn_object argument 0
-#define NAVI                0x8022CBE0
+#define Z64GL_SAVE_CONTEXT          0x8015E660
+#define STATIC_CONTEXT              0x8015FA90
+#define RAM_SEGMENT_TABLE           0x80166FA8
+#define GLOBAL_CONTEXT              0x80212020
+#define AS_A0                       (GLOBAL_CONTEXT + 0x001C24) // spawn_actor argument 0
+#define OS_A0                       (GLOBAL_CONTEXT + 0x0117A4) // spawn_object argument 0
+#define NAVI                        0x8022CBE0
+
+#include "shared.h"
 
 #endif
