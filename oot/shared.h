8324c4b67d98223d6f1e50205d567466fdf11c9c
z64me <45853441+z64me@users.noreply.github.com>
2019-10-09 09:14:13 -0400

    `p a r e n t h e s i s`

--- oot/shared.h
+++ oot/shared.h
@@ -3,8 +3,8 @@
 
 /* relative offsets that remain the same between versions */
 
-#define Z64GL_INTERFACE_ON          (Z64GL_SAVE_CONTEXT + 0x135F) /* byte */
-#define Z64GL_CUTSCENE_PLAY_SCRIPT  (Z64GL_SAVE_CONTEXT + 0x1414) /* byte */
-#define Z64GL_IS_NIGHT              (Z64GL_SAVE_CONTEXT + 0x0010) /* u32, 0 = day, 1 = night */
+#define Z64GL_INTERFACE_ON          ((Z64GL_SAVE_CONTEXT) + 0x135F) /* byte */
+#define Z64GL_CUTSCENE_PLAY_SCRIPT  ((Z64GL_SAVE_CONTEXT) + 0x1414) /* byte */
+#define Z64GL_IS_NIGHT              ((Z64GL_SAVE_CONTEXT) + 0x0010) /* u32, 0 = day, 1 = night */
 
 #endif /* __Z64OVL_OOT_SHARED_H__ */
