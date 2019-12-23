57866fa3cb3ce3a0ae4675b0160a67b3c5daa6ba
Zelllll <56516451+Zelllll@users.noreply.github.com>
2019-12-22 20:05:47 -0600

    Update u10.h

--- mm/u10.h
+++ mm/u10.h
@@ -9,11 +9,11 @@
 #include "../z64_functions.h"
 
 // RAM Stuff
-#define SAVE_CONTEXT      0x801EF670
-#define GLOBAL_IS_NIGHT   0x801EF680 // u32, 0 if day, 1 if night
-#define RAM_SEGMENT_TABLE 0x801F8180 // 0x80100504 is like rel2abs
-#define GLOBAL_CONTEXT    0x803E6B20
-#define AS_A0             (GLOBAL_CONTEXT + 0x001CA0) // spawn_actor argument 0
-#define OS_A0             (GLOBAL_CONTEXT + 0x017D88) // spawn_object argument 0
+#define Z64GL_SAVE_CONTEXT  0x801EF670
+#define Z64GL_IS_NIGHT      0x801EF680 // u32, 0 if day, 1 if night
+#define RAM_SEGMENT_TABLE   0x801F8180 // 0x80100504 is like rel2abs
+#define GLOBAL_CONTEXT      0x803E6B20
+#define AS_A0               (GLOBAL_CONTEXT + 0x001CA0) // spawn_actor argument 0
+#define OS_A0               (GLOBAL_CONTEXT + 0x017D88) // spawn_object argument 0
 
 #endif
