fef39a307d29cf6d669d204a931f300f8ea1b41f
z64me <45853441+z64me@users.noreply.github.com>
2020-02-29 05:23:12 -0500

    `z_skelanime_draw` versatility

--- z64_functions.h
+++ z64_functions.h
@@ -4,6 +4,7 @@
 #include "h/ichain.h"
 #include "common.h"
 #include "oldnames.h"
+#include "real.h"
 
 /****
  * z64ovl.h
@@ -5797,79 +5798,53 @@ extern void external_func_800A0B40(void);
 	#endif
 
 /* Draw a `skelanime` structure.
-* This does not support `matrix-enabled` skeletons.
+* This function supports both matrix and non-matrix formats; specify
+* the number of limbs using matrices for a matrix format, 0 otherwise.
 * Debug strings dub this function `Si2_draw()` (Skeleton Info 2; Draw)?
 * Source Code Reference File: "z_skelanime.c"
 * Formerly `skelanime_draw`
 */
-extern void z_skelanime_draw(
-z64_global_t* gl /* Global Context */
-, uint32_t limb_index /* Skelanime Limb Index */
-, void* dtstart /* Skelanime Draw Table Start */
-, int32_t callback0( /* This must return 0 */
-	z64_global_t* gl /* Global Context */
-	, int32_t limb /* Current Limb */
-	, uint32_t* dlist /* Display List Pointer */
+static inline void z_skelanime_draw(
+z64_global_t* gl       /* Global Context */
+, int matrix_limbs     /* Number of matrix limbs, or 0 if non-matrix */
+, void* instance       /* A pointer to the actor instance */
+, z64_skelanime_t *sk  /* Skelanime */
+, int32_t callback0(      /* This must return 0 */
+	z64_global_t* gl       /* Global Context */
+	, int32_t limb         /* Current Limb */
+	, uint32_t* dlist      /* Display List Pointer */
 	, vec3f_t* translation /* Limb Translation */
-	, vec3s_t* rotation /* Limb Rotation */
-	, void* instance /* Current instance pointer (entity_t) */
+	, vec3s_t* rotation    /* Limb Rotation */
+	, void* instance       /* Current instance pointer (entity_t) */
 	)
-, void callback1( /* An internal callback function. */
-	z64_global_t* gl /* Global Context */
-	, int32_t limb /* Current Limb */
-	, uint32_t dlist /* Display List */
-	, vec3s_t* rotation /* Limb Rotation */
-	, void* instance /* Current instance pointer (entity_t) */
+, void callback1(         /* An internal callback function. */
+	z64_global_t* gl       /* Global Context */
+	, int32_t limb         /* Current Limb */
+	, uint32_t dlist       /* Display List */
+	, vec3s_t* rotation    /* Limb Rotation */
+	, void* instance       /* Current instance pointer (entity_t) */
 	)
-, void* instance /* A pointer to the actor instance containing the skelanime structure. */
-);
-#if OOT_DEBUG
-  asm("z_skelanime_draw = 0x800A15C8");
-#elif OOT_U_1_0
-  asm("z_skelanime_draw = 0x8008993C");
-#elif MM_U_1_0
-  asm("z_skelanime_draw = 0x80133B3C");
-#endif
-
-/**
- * Draw Matrix-Enabled Object
- * callback0 and callback1 should be NULL, or pointers
-   to functions of their respective descriptions
- * TODO Variable name cleanup, better notes
- * A0 = Global Context | A1 = Hierarchy Limb Index (in Object File, in RAM) | A2 = Pointer to Actor Drawing Table | A3 = Number of Limbs that use display lists. | 0x0010(SP) = 0 | 0x0014(SP) = 0 | 0x0018(SP) = Actor Instance Address
- * TODO confirm dlist argument of callback0
- */
-extern
-void
-skelanime_draw_mtx(
-  z64_global_t *global
-, uint32_t limb_index
-, uint32_t adt
-, uint8_t limb_dlists_count
-, int32_t callback0(            /* callback0 must return 0 */
-	  z64_global_t *global
-	, uint8_t limb           /* limb index in skeleton */
-	, uint32_t *dlist        /* *dlist = 0x06xxxxxx changes limb model */
-	, vec3f_t *translation
-	, vec3s_t *rotation
-	, void *entity
-  )
-, void callback1(
-	  z64_global_t *global
-	, uint8_t limb
-	, uint32_t dlist         /* different from callback0, needs more research */
-	, vec3s_t *rotation
-	, void *entity
-  )
-, void *entity
-);
-	#if OOT_DEBUG
-		asm("skelanime_draw_mtx = 0x800A1AC8");
-	#elif OOT_U_1_0
-		asm("skelanime_draw_mtx = 0x80089D8C");
-	#elif MM_U_1_0
-		asm("skelanime_draw_mtx = 0x80133F28");
-	#endif
+) {
+	if (matrix_limbs)
+		_z_skelanime_draw_mtx(
+			gl
+			, sk->limb_index
+			, sk->draw_table_rot
+			, matrix_limbs
+			, callback0
+			, callback1
+			, instance
+		);
+	else
+		_z_skelanime_draw(
+			gl
+			, sk->limb_index
+			, sk->draw_table_rot
+			, callback0
+			, callback1
+			, instance
+		);
+}
 
 /* Return Animation Frame Count
 * Source Code Reference File: "z_skelanime.c"
