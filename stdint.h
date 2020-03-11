8013b17326115d3e875bee0f9544a5876f27095f
z64me <45853441+z64me@users.noreply.github.com>
2020-03-11 13:35:15 -0400

    `stdint.h`: add `intptr_t` and `uintptr_t`

--- stdint.h
+++ stdint.h
@@ -10,12 +10,14 @@ typedef signed char          int8_t;
 typedef signed short int     int16_t;
 typedef signed int           int32_t;
 typedef signed long long     int64_t;
+typedef signed int           intptr_t;
 
 /* Unsigned.  */
 typedef unsigned char        uint8_t;
 typedef unsigned short int   uint16_t;
 typedef unsigned int         uint32_t;
 typedef unsigned long long   uint64_t;
+typedef unsigned int         uintptr_t;
 
 typedef __SIZE_TYPE__        size_t;
 
