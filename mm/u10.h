6ceb6181fa81691602e6c8140ca1885615be58cf
Zelllll <elijah@DESKTOP-9LCF2VJ.localdomain>
2020-03-16 20:05:07 -0500

    MM actors should now compile.

--- mm/u10.h
+++ mm/u10.h
@@ -1,13 +1,10 @@
 #ifndef __Z64OVL_MM_U_10_H__
 #define __Z64OVL_MM_U_10_H__
 
+#define GAME_MM 1
 #define MM_U_1_0 1
 #define NZSE 1
 #define RETAIL_NUANCES 1
-
-#include "../h/mips.h"
-#include "types.h"
-#include "../z64_functions.h"
 
 // RAM Stuff
 #define Z64GL_SAVE_CONTEXT                  0x801EF670
@@ -17,5 +14,9 @@
 #define GLOBAL_CONTEXT                      0x803E6B20
 #define AS_A0                               (GLOBAL_CONTEXT + 0x001CA0) // spawn_actor argument 0
 #define OS_A0                               (GLOBAL_CONTEXT + 0x017D88) // spawn_object argument 0
+
+#include "../h/mips.h"
+#include "types.h"
+#include "../z64_functions.h"
 
 #endif
