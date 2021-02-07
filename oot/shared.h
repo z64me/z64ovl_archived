fc21cfd48c846591ed4b46be2f478732116f85b0
z64me <45853441+z64me@users.noreply.github.com>
2021-02-07 14:43:30 -0500

    Update shared.h

--- oot/shared.h
+++ oot/shared.h
@@ -6,5 +6,6 @@
 #define Z64GL_INTERFACE_ON          ((Z64GL_SAVE_CONTEXT) + 0x135F) /* byte */
 #define Z64GL_CUTSCENE_PLAY_SCRIPT  ((Z64GL_SAVE_CONTEXT) + 0x1414) /* byte */
 #define Z64GL_IS_NIGHT              ((Z64GL_SAVE_CONTEXT) + 0x0010) /* u32, 0 = day, 1 = night */
+#define Z64GL_PAUSEPLAYER           (Z64GL_CODE + Z64GL_CODE_PAUSEPLAYER)
 
 #endif /* __Z64OVL_OOT_SHARED_H__ */
