497490deb9abb2d093b74b27f8b3516733aa4814
z64me <45853441+z64me@users.noreply.github.com>
2021-02-09 12:06:13 -0500

    `Z64GL_INTERFACE_DISABLED`

--- oot/shared.h
+++ oot/shared.h
@@ -3,7 +3,7 @@
 
 /* relative offsets that remain the same between versions */
 
-#define Z64GL_INTERFACE_ON          ((Z64GL_SAVE_CONTEXT) + 0x135F) /* byte */
+#define Z64GL_INTERFACE_DISABLED    (*(unsigned char*)((Z64GL_SAVE_CONTEXT) + 0x135F)) /* byte */
 #define Z64GL_CUTSCENE_PLAY_SCRIPT  ((Z64GL_SAVE_CONTEXT) + 0x1414) /* byte */
 #define Z64GL_IS_NIGHT              ((Z64GL_SAVE_CONTEXT) + 0x0010) /* u32, 0 = day, 1 = night */
 #define Z64GL_PAUSEPLAYER           (Z64GL_CODE + Z64GL_CODE_PAUSEPLAYER)
